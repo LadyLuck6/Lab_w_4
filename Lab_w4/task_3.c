76f9d5cae031d9ffb9ec2746b57f159ebdd63b2b
LadyLuck6 <129070816+LadyLuck6@users.noreply.github.com>
2023-04-24 14:15:49 +0300

    Update task_3.c

--- Lab_w4/task_3.c
+++ Lab_w4/task_3.c
@@ -1,16 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    int i, j, product, result = 0;
-    // product - для зберігання добутку внутрішнього циклу
-    //  аresult - для зберігання суми всіх добутків.
-    for (i = 1; i <= 10; i++) {
-        product = 1;
-    for (j = 1; j <= i; j++) {
-            product *= (i + j);
-        }
-        result += product;
+    int i, j, s, p;
+    for (i = 1; i <= 10; i++) 
+   { for (j = 1; j <= i; j++) 
+    s*=(i+j);
+    p+=s;
     }
-    printf("Result: %d\n", result);
+    printf("p=%d\n");
     return 0;
 }
