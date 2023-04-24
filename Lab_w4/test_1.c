341a4e6462a4157882ef7884736bd9d195ec8f9b
LadyLuck6 <129070816+LadyLuck6@users.noreply.github.com>
2023-04-24 13:39:13 +0300

    Update test_1.c

--- Lab_w4/test_1.c
+++ Lab_w4/test_1.c
@@ -1,10 +1,18 @@
-#include <stdio.h>
-
 int main() {
-  int s = 0;
-for (int i = 1; i <= 20; i++)
-s += i;
-printf("Suma = %d\n ", s);
+    float a = 0.5;
+    float b = 4.5;
+    float dx = 0.4;
+    float x, y, result;
+    printf("x\t\t| f(x)\n");
+    printf("--------------------------------\n");
+    for (x = a; x <= b; x += dx) {
+        // Обчислюємо значення функції з урахуванням виключних ситуацій
+        if (x <= 0) {
+            printf("%.2f\t| Невизначено\n", x);
+        } else {
+            y = log(x);
+            printf("%.2f\t| %.2f\n", x, y);
+        }
+    }
     return 0;
 }
-
